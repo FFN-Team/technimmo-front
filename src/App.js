45441d9cc9288eb78b3f994d8e8231f2b27410c4
Niraiksan <103355201+Niraiksan@users.noreply.github.com>
2024-02-13 11:35:44 +0100

    Revert "Feature/ti 102 second step front for prospects search"

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-// project import
-import Routes from 'routes';
-import ThemeCustomization from 'themes';
-import ScrollTop from 'components/ScrollTop';
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-// ==============================|| APP - THEME, ROUTER, LOCAL  ||============================== //
+class App extends Component {
+  state = {
+    get : {}
+  }
 
-const App = () => (
-  <ThemeCustomization>
-    <ScrollTop>
-      <Routes />
-    </ScrollTop>
-  </ThemeCustomization>
-);
+  componentDidMount(){
+    fetch('localhost:9001/bien')
+    .then((response) => {
+      return response.json()
+    })
+    .then((result) => {
+      this.setState({get: result})
+    })
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Biens : </h1>
+        {this.state.get.nomBien}
+      </div>
+    );
+  }
+}
 
 export default App;
