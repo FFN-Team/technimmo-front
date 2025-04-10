253b17ebc7cbaa0b07617dd5503f32ee37e8121a
Florine Philippe <fphilippe78480@gmail.com>
2025-04-10 08:19:23 +0200

    WIP

--- src/menu-items/dashboard.js
+++ src/menu-items/dashboard.js
@@ -1,24 +1,42 @@
 // assets
-import { DashboardOutlined } from '@ant-design/icons';
+import { DashboardOutlined, BookOutlined, LikeOutlined } from '@ant-design/icons';
 
 // icons
 const icons = {
-  DashboardOutlined
+  DashboardOutlined,
+  BookOutlined,
+  LikeOutlined
 };
 
 // ==============================|| MENU ITEMS - DASHBOARD ||============================== //
 
 const dashboard = {
-  id: 'group-dashboard',
-  title: 'Navigation',
+  id: 'group-ads',
+  title: 'Annonces',
   type: 'group',
   children: [
     {
-      id: 'dashboard',
-      title: 'Dashboard',
+      id: 'ads-data-analytics',
+      title: 'Analyse des annonces',
       type: 'item',
-      url: '/dashboard',
+      url: '/analyse-annonce',
       icon: icons.DashboardOutlined,
+      breadcrumbs: false
+    },
+    {
+      id: 'ads-interactive-map',
+      title: 'Carte intéractive des annonces',
+      type: 'item',
+      url: '/carte-interactive-annonces',
+      icon: icons.BookOutlined,
+      breadcrumbs: false
+    },
+    {
+      id: 'ads-recommendation',
+      title: 'Recommandation d\'annonces',
+      type: 'item',
+      url: '/recommandation-annonces',
+      icon: icons.LikeOutlined,
       breadcrumbs: false
     }
   ]
--- src/pages/property/index.js
+++ src/pages/property/index.js
@@ -69,8 +69,8 @@ useEffect(() => {
     <div className="property-details">
       <Tabs className="custom-tabs" selectedIndex={activeTab} onSelect={handleTabSelect}>
         <TabList className="custom-tab-list">
-          <Tab className={`custom-tab ${activeTab === 0 ? 'active' : ''}`}>Informations du bien</Tab>
-          <Tab className={`custom-tab ${activeTab === 1 ? 'active' : ''}`}>Gestion des documents</Tab>
+          <Tab className={`custom-tab ${activeTab === 0 ? 'active' : ''}`}>🏠 Informations du bien</Tab>
+          <Tab className={`custom-tab ${activeTab === 1 ? 'active' : ''}`}>📄 Gestion des documents</Tab>
         </TabList>
 
         <TabPanel>
